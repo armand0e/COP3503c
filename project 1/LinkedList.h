47377278be78d3c9b0b8d770170115de5a8b6642
armand0e <53029190+armand0e@users.noreply.github.com>
2024-06-15 23:11:26 -0400

    update proj1

--- project 1/LinkedList.h
+++ project 1/LinkedList.h
@@ -1,213 +1,356 @@
 #pragma once
 #include <iostream>
-#include <stdexcept>
 #include <vector>
+#include <string>
 using namespace std;
 
-template <typename T> class LinkedList {
+template <typename T> 
+class LinkedList {
 public:
-    // NestedClass
+    //////////////////////////////// Nested Class /////////////////////////////////
     struct Node {
         T data;
-        Node* next = nullptr;
-        Node* prev = nullptr;
+        Node* next;
+        Node* prev;
         Node(T data) : data(data), next(nullptr), prev(nullptr) {}
     };
+
     ////////////////////////////////// Behaviors //////////////////////////////////
-    void PrintForward()  {
-        // Iterator through all of the nodes and print out their values, one a time
-        if (node_count > 0) {
-            // set loop starting point
-            Node* currentNode = head;
-            // break loop when currentNode points to nullptr
-            while (currentNode != nullptr) {
-                // print the data
-                cout << currentNode->data << endl;
-                // point the currentNode to the next node
-                currentNode = currentNode->next;                
-            }
-        }
-    }
-    void PrintReverse() const {
-        //Exactly the same as PrintForward, except completely the opposite.
-        if (node_count > 0) {
-            // set loop starting point
-            Node* currentNode = tail;
-            // break loop when currentNode points to nullptr
-            while (currentNode != nullptr) {
-                // print the data
-                cout << currentNode->data << endl;
-                // point the currentNode to the next node
-                currentNode = currentNode->prev;                
-            }
-        }
-    }
-    /* void PrintForwardRecursive(const Node* node) const {
-        This function takes in a pointer to a Node—a starting node. From
-        that node, recursively visit each node that follows, in forward order,
-        and print their values. This function MUST be implemented using
-        recursion, or tests using it will be worth no points. Check your
-        textbook for a reference on recursion.
-
-    }
-    void PrintReverseRecursive(const Node* node) const {
-        //Same deal as PrintForwardRecursive, but in reverse.
-
-    } */
+    void PrintForward() const;
+    void PrintReverse() const;
+    void PrintForwardRecursive(const Node* node) const;
+    void PrintReverseRecursive(const Node* node) const;
 
     ////////////////////////////////// Accessors //////////////////////////////////
-    unsigned int NodeCount() const {
-        return node_count;
-    }
-    /* void FindAll(vector<Node*>& outData, const T& value) const {
-
-    }
-    const Node* Find(const T& data) const {
-
-    }
-    const Node* GetNode(unsigned int index) const {
-
-    }
-    Node* GetNode(unsigned int index) {
-
-    }
-    Node* Head() {
-        return head;
-    }
-    //const Node* Head() const {
-    //    return const head;
-    }
-    Node* Tail() {
-        return tail;
-    }
-    //const Node* Tail() const {
-    //    return tail;
-    } */
+    unsigned int NodeCount() const;
+    void FindAll(vector<Node*>& outData, const T& value) const;
+    const Node* Find(const T& data) const;
+    const Node* GetNode(unsigned int index) const;
+    Node* GetNode(unsigned int index);
+    Node* Head();
+    const Node* Head() const;
+    Node* Tail();
+    const Node* Tail() const;
 
     ////////////////////////////////// Insertion //////////////////////////////////
-    void AddHead(const T& newdata) {
-        
-        Node newHead = new Node;
-        Node* newHeadptr = &newHead;
-        newHead->data = newdata;
-        newHead->next = head;
-        head->prev = newHeadptr;
-        head = newHeadptr;
-        if (tail == nullptr) tail = newHeadptr;
-        node_count++;
-    }
-     void AddTail(const T& newdata) {
-        Node newTail = new Node;
-        Node* newTailptr = &newTail;
-        newTail->data = newdata;
-        newTail->prev = tail;
-        tail->next = newTailptr;
-        tail = newTailptr;
-        if (head == nullptr) head = newTailptr;
-        node_count++;
-    }
-    void AddNodesHead(const T* data, unsigned int count) {
-        for (int i = count - 1; i >= 0; i--) {
-            AddHead(data[i]);
-            node_count++;
-        }
-    }
-    void AddNodesTail(const T* data, unsigned int count) {
-        for (int i = 0; i < count; i++) {
-            AddTail(data[i]);
-            node_count++;
-        }
-    }
-    /* void InsertAfter (Node* node, const T& data) {
-
-    } 
-    void InsertBefore(Node* node, const T& data) {
-        
-    }
-    void InsertAt (const T& data, unsigned int index) {
-
-    } */
+    void AddHead(const T& data);
+    void AddTail(const T& data);
+    void AddNodesHead(const T* data, unsigned int count);
+    void AddNodesTail(const T* data, unsigned int count);
+    void InsertAfter (Node* node, const T& data);
+    void InsertBefore(Node* node, const T& data);
+    void InsertAt (const T& data, unsigned int index);
 
     /////////////////////////////////// Removal ///////////////////////////////////
-    /* bool RemoveHead(); bool RemoveTail() {
-        
-    }
-    int Remove(const T&data) {
-
-    } 
-    bool RemoveAt (unsigned int index) {
-
-    } */
-    void Clear() {
-        Node* currentNode = Head();
-        Node* nextNode = nullptr;
-        while (currentNode!=nullptr) {
-            nextNode = currentNode->next;
-            delete currentNode;
-            currentNode = nextNode;
-        }
-    }
+    bool RemoveHead(); 
+    bool RemoveTail();
+    unsigned int Remove(const T&data); 
+    bool RemoveAt (unsigned int index);
+    void Clear();
 
     ////////////////////////////////// Operators //////////////////////////////////
-    /* const T& operator[](unsigned int index) const {
+    const T& operator[](unsigned int index) const;
+    T& operator[](unsigned int index);
+    bool operator==(const LinkedList<T>& rhs) const;
+    LinkedList<T>& operator=(const LinkedList<T>& rhs);
 
-    }
-    T& operator[](unsigned int index) {
-
-    }
-    bool operator==(const LinkedList<T>& rhs) const {
-
-    }
-    LinkedList<T>& operator=(const LinkedList<T>& rhs) {
-
-    } */
-
-    /////////////////////////// Construction/Destruction ///////////////////////////
-    LinkedList() {
-        node_count = 0;
-        head = nullptr;
-        tail = nullptr;
-    }
-    /* LinkedList(const LinkedList<T>& list) {               
-        node_count = list->node_count;
-        Node* currentlistNode = list->head;
-        while (currentlistNode != nullptr) {
-            // create a new node identical to the current list node
-            AddTail(currentlistNode->data);
-            // point the currentlistNode to the next list node
-            currentlistNode = currentlistNode->next;
-        }
-        delete currentlistNode;
-        head = Head();
-        tail = Tail();
-
-    } */
-    ~LinkedList() {
-        if (node_count > 0) {
-            // set loop starting point
-            Node* currentNode = head;
-            
-            while (node_count > 0) {
-                // store the pointer to the next node
-                Node* nextNode = currentNode->next;
-                // deconstruct current node
-                delete currentNode->data;
-                delete currentNode->next;
-                delete currentNode->prev;
-                // point the currentNode to the next node
-                currentNode = nextNode;
-                delete nextNode;
-                // break if you reach nullptr
-                if (currentNode == nullptr) break;
-                node_count--;
-            }
-        }
-    }
+    ////////////////////////// Construction/Destruction ///////////////////////////
+    LinkedList();
+    LinkedList(const LinkedList<T>& list);
+    ~LinkedList();
 
 private:
-    Node* head = nullptr;
-    Node* tail = nullptr;
+    Node *head;
+    Node *tail;
     unsigned int node_count;
 };
 
+/*------------------------- FUNCTION DEFINITIONS --------------------------------*/
 
+//////////////////////////////////// Behaviors ////////////////////////////////////
+template <typename T>
+void LinkedList<T>::PrintForward() const {
+    // Traverse the list and print each node's data
+    Node* current = this->head;
+    while (current->next != nullptr) {
+        cout << current->data << endl;
+        current = current->next;
+    }
+    cout << current->data << endl;
+}
+template <typename T>
+void LinkedList<T>::PrintReverse() const {
+    // Traverse the list and print each node's data
+    Node* current = this->tail;
+    while (current->prev != nullptr) {
+        cout << current->data << endl;
+        current = current->prev;
+    }
+    cout << current->data << endl;
+    
+}
+//void PrintForwardRecursive(const Node* node) const;
+//void PrintReverseRecursive(const Node* node) const;
+
+//////////////////////////////////// Accessors ////////////////////////////////////
+template <typename T>
+unsigned int LinkedList<T>::NodeCount() const {
+    return node_count;
+}
+template <typename T>
+void LinkedList<T>::FindAll(vector<Node*>& outData, const T& value) const {
+    Node* current = head;
+    while (current != nullptr) {
+        if (current->data == value) {
+            outData.push_back(current);
+        }
+        current = current->next;
+    }
+}
+template <typename T>
+const typename LinkedList<T>::Node* LinkedList<T>::Find(const T& data) const {
+    Node* current = head;
+    while (current != nullptr) {
+        if (current->data == data) {
+            return current;
+        }
+        current = current->next;
+    }
+    return nullptr;
+}
+template <typename T>
+typename LinkedList<T>::Node* LinkedList<T>::GetNode(unsigned int index) {
+    if (index >= node_count) {
+        throw out_of_range("Error: List index out of range!");
+    }
+    Node* current = head;
+    for (unsigned int i = 0; i < index; ++i) {
+        current = current->next;
+    }
+    return current;
+}
+template <typename T>
+const typename LinkedList<T>::Node* LinkedList<T>::GetNode(unsigned int index) const {
+    if (index >= node_count) {
+        throw out_of_range("Error: List index out of range!");
+    }
+    Node* current = head;
+    for (unsigned int i = 0; i < index; ++i) {
+        current = current->next;
+    }
+    return current;
+}
+template <typename T>
+typename LinkedList<T>::Node* LinkedList<T>::Head() {
+    return head;
+}
+
+template <typename T>
+const typename LinkedList<T>::Node* LinkedList<T>::Head() const {
+    return head;
+}
+
+template <typename T>
+typename LinkedList<T>::Node* LinkedList<T>::Tail() {
+    return tail;
+}
+
+template <typename T>
+const typename LinkedList<T>::Node* LinkedList<T>::Tail() const {
+    return tail;
+}
+
+//////////////////////////////////// Insertion ////////////////////////////////////
+template <typename T>
+void LinkedList<T>::AddHead(const T& data) {
+    Node* newNode = new Node(data);
+
+    if (head == nullptr) {
+        head = newNode;
+        tail = newNode;
+    } 
+    else {
+        newNode->next = head;
+        head->prev = newNode;
+        head = newNode;
+    }
+    node_count++;
+}
+template <typename T>
+void LinkedList<T>::AddTail(const T& data) {
+    Node* newNode = new Node(data);
+
+    if (head == nullptr) {
+        head = newNode;
+        tail = newNode;
+    } 
+    else {
+        newNode->prev = tail;
+        tail->next = newNode;
+        tail = newNode;
+    }
+    node_count++;
+}
+template <typename T>
+void LinkedList<T>::AddNodesHead(const T* data, unsigned int count) {
+    for (int i = count - 1; i >= 0; i--) {
+        AddHead(data[i]);
+        node_count++;
+    }
+}
+template <typename T>
+void LinkedList<T>::AddNodesTail(const T* data, unsigned int count) {
+    for (int i = 0; i < count; i++) {
+        AddTail(data[i]);
+        node_count++;
+    }
+}
+template <typename T>
+void LinkedList<T>::InsertAfter (Node* node, const T& data) {
+    Node* newNode = new Node(data);
+    // if given node is the tail
+    if (node == tail) {
+        node->next = newNode;
+        newNode->prev = node;
+        tail = newNode;
+    }
+    //otherwise
+    else {
+        Node* temp = node->next;
+        node->next = newNode;
+        newNode->prev = node;
+        newNode->next = temp;
+        temp->prev = newNode;
+    }
+}
+template <typename T>
+void LinkedList<T>::InsertBefore(Node* node, const T& data) {
+    Node* newNode = new Node(data);
+    // if given node is the head
+    if (node == head) {
+        node->prev = newNode;
+        newNode->next = node;
+        head = newNode;
+    }
+    //otherwise
+    else {
+        Node* temp = node->prev;
+        node->prev = newNode;
+        newNode->next = node;
+        newNode->prev = temp;
+        temp->next = newNode;
+    }
+}
+template <typename T>
+void LinkedList<T>::InsertAt (const T& data, unsigned int index) {
+    if (index >= node_count) {
+        throw out_of_range("Error: List index out of range.");
+    }
+    // if inserting at the head
+    if (index == 0) {
+        AddHead(data);
+    }
+    // if inserting at the tail
+    else if (index == node_count-1) {
+        AddTail(data);
+    }
+    // otherwise
+    else {
+        InsertAfter(GetNode(index-1), data);
+    }
+}
+
+///////////////////////////////////// Removal /////////////////////////////////////
+//bool RemoveHead(); 
+//bool RemoveTail();
+//unsigned int Remove(const T&data); 
+//bool RemoveAt (unsigned int index);
+template <typename T>
+void LinkedList<T>::Clear() {
+    // Traverse the list and delete each node to free memory
+    Node* current = head;
+    while(current != nullptr) {
+        // assign temp to current
+        Node* temp = current;
+        // assign current to next
+        current = current->next;
+        // delete current
+        delete temp;
+    }
+    head = nullptr;
+    tail = nullptr;
+    node_count = 0;
+}
+
+//////////////////////////////////// Operators ////////////////////////////////////
+template <typename T>
+const T& LinkedList<T>::operator[](unsigned int index) const {
+    return GetNode(index)->data;
+}
+
+template <typename T>
+T& LinkedList<T>::operator[](unsigned int index) {
+    return GetNode(index)->data;
+}
+template <typename T>
+bool LinkedList<T>::operator==(const LinkedList<T>& rhs) const {
+    if (this == &rhs) {
+        return true; // same object
+    }
+
+    if (node_count != rhs.node_count) {
+        return false; // different sizes
+    }
+
+    Node* current_self = head;
+    Node* current_rhs = rhs.head;
+
+    while (current_self != nullptr) {
+        if (current_self->data != current_rhs->data) {
+            return false; // different data
+        }
+        current_self = current_self->next;
+        current_rhs = current_rhs->next;
+    }
+
+    return true; // all nodes are identical
+}
+template <typename T>
+LinkedList<T>& LinkedList<T>::operator=(const LinkedList<T>& rhs) {
+    if (this != &rhs) {  // Self-assignment check
+        // first delete all nodes
+        Clear();
+        // copy each node from the provided list
+        Node* current = rhs.head;
+        while (current != nullptr) {
+            AddTail(current->data);
+            current = current->next;
+        }
+    }
+    return *this;
+}
+
+//////////////////////////// Construction/Destruction /////////////////////////////
+template <typename T>
+LinkedList<T>::LinkedList() {
+    node_count = 0;
+    head = nullptr;
+    tail = nullptr;
+}
+template <typename T>
+LinkedList<T>::LinkedList(const LinkedList<T>& list) {
+    node_count = 0;
+    head = nullptr;
+    tail = nullptr;
+    // copy each node from the provided list
+    Node* current = list.head;
+    while (current != nullptr) {
+        AddTail(current->data);
+        current = current->next;
+    }
+}
+
+template <typename T>
+LinkedList<T>::~LinkedList() {
+    Clear();
+}
